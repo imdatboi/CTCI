017d2e3a03ddcea6a9372c8f01cf8e1b4fd9022f
imdatboi <fuentesjorge42@gmail.com>
2018-06-20 23:52:40 -0400

    bug fixed, could still use dp

--- ch8/CTCI_8_2.java
+++ ch8/CTCI_8_2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class MyClass {
     public static void main(String args[]) {
-        int[][] grid = {{1,0,0,0}, {1,1,0,0}, {0,1,1,1}, {0,0,0,1}};  
+        int[][] grid = {{1,1,0,0}, {1,1,0,0}, {0,1,1,1}, {0,0,0,1}};  
         ArrayList<String> ans = gridPath(grid); 
         
         for(int i=0; i<ans.size(); i++) { 
@@ -19,8 +19,9 @@ public class MyClass {
     }
     
     public static void helper(int[][] grid, ArrayList<String> path, int i, int j) { 
+        if(path.size() >= 1 && path.get(path.size()-1).equals("["+ (grid.length-1) + "][" + (grid[0].length-1) +"]")) return; 
         if(i == grid.length-1 && j == grid[0].length-1) { 
-            path.add("["+ i + "][" + j +"]");    
+            path.add("["+ i + "][" + j +"]"); 
             return; 
         }
         if(i >= grid.length || j >= grid[0].length || grid[i][j] == 0) { 
